205365661fadb273ac46c2b931ae140052698f80
Filip Drzewiecki <filip.drzewiecki@yahoo.com>
2022-02-28 20:05:20 +0000

    pom od Matiego

--- src/main/java/pl/pandait/panda/HelloController.java
+++ src/main/java/pl/pandait/panda/HelloController.java
@@ -7,13 +7,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class HelloController{
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name",
-		required=false, defaultValue="World")
-		String name, Model model){
-		model.addAttribute("name", name);
-		return"greeting";
-	}
+public class HelloController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting_template";
+    }
 }
 
